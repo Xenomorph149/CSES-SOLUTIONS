13efa9644a36864a12b6b37ff88de85094b5d11f
preetham <nvpreetham99@gmail.com>
2026-01-17 18:20:05 +0530

    commit no.1

---
+++ Introductory Problems/WierdAlgorithm.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <functional>
+#include <unordered_map>
+#include <unordered_set>
+#include <limits>
+using namespace std;
+#define int long long
+#define double long double
+#define mod 1000000007
+#define pi 3.1415926535897932384626433832795
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+ 
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+vector < int > read_arr(int n) {
+    vector < int > arr(n);
+    for (int & x: arr) cin >> x;
+    return arr;
+}
+void print_arr(const vector < int > & arr) {
+    for (int x: arr) cout << x << " ";
+    cout << endl;
+}
+vector < int > SieveOfEratosthenes(int n) {
+    vector < bool > prime(n + 1, true);
+    vector < int > primes;
+    for (int p = 2; p * p <= n; ++p)
+        if (prime[p])
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+    for (int p = 2; p <= n; ++p)
+        if (prime[p]) primes.push_back(p);
+    return primes;
+}
+signed main() {
+    setIO();
+    int t=1, n;
+    while (t--) {
+        cin >> n;
+        while(n!=1){
+            cout<<n<<" ";
+            if(n&1){
+                n*=3;
+                n++;
+            }
+            else{
+                n/=2;
+            }
+        }
+        cout<<n<<endl;
+    }
+    return 0;
+}
